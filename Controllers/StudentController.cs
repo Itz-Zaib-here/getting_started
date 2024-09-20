06b95bee1c69b3e5534643c3e310ba1e43f376e7
Shah Zaib <shahzaibshazada123@gmail.com>
2024-09-20 02:03:09 -0700

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace getting_started.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentBio()
+        {
+            return View();
+        }
+    }
+}
